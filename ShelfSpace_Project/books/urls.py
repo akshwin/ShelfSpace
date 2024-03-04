12c5448b62a2912deb3faf3ed79c3451e0c8968c
Ravin D <ravin.d3107@outlook.com>
2024-03-04 09:50:46 +0530

    Made Changes to upload in Backend

---
+++ ShelfSpace_Project/books/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import file_book, upload_book, download_book, delete_book, view_book, share_book
+
+app_name= 'books'
+urlpatterns = [
+    path('', file_book, name='file_list'),
+    path('upload/', upload_book, name='upload_file'),
+    path('download/<str:unique_token>/', download_book, name='download_file'),
+    path('delete/<str:unique_token>/', delete_book, name='delete_file'),
+    path('view_file/<str:unique_token>/', view_book, name='view_file'),
+    path('share_file/<str:unique_token>/', share_book, name='share_file'),
+]
